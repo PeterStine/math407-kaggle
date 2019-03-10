72eca60db424a54e12a8e9ca9dc283e69ffbb4b1
Peter Stine <peter.stine@oit.edu>
2019-03-09 21:43:02 -0800

    Reading in and describing the dataset with pandas

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # Digit Recognizer kaggle competition main file
 
 # Imports
+import numpy as np
+import pandas as pd
+import sys # For adding 
 
 # Major function calls
+digits_dataset = pd.read_csv('train.csv', sep=",")
+print(digits_dataset.describe())
